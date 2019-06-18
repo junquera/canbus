b097b1bdce6e401c07d2f166f517e0d567141202
J. Junquera <javier.junquera.sanchez@protonmail.com>
2019-06-18 23:38:47 +0200

    Main

--- main.js
+++ main.js
@@ -1,68 +1,136 @@
-// class Monitor {
-//
-// }
-
-class Event {
-  constructor(name, callback){
-    this.name = name;
-    this.callback = callback;
+function newId(){
+  var res = "";
+  for(let i=0; i< 8; i++){
+    res += Math.random() > 0.5? 1 : 0;
   }
 
-  launch(c){
-    this.callback(c);
+  return res;
+}
+
+class Component {
+
+  constructor (name, id) {
+    this.id = id ? id : newId();
+    this.name = name;
+    this.events = {};
+  }
+
+  setCon (con) {
+    this.con = con;
+  }
+
+  addEvent(command, e){
+    this.events[command.code] = e;
+  }
+
+  w(command){
+    if(command.id == this.id)
+      if(this.events[command.code])
+        this.events[command.code](this, command.extra);
+  }
+
+  send(command){
+    this.con.w(command);
+  }
+
+  toString(){
+    return this.name + this.id
   }
 }
+
+
+puerta_izda = new Component("puerta_izda");
+pantalla = new Component("pantalla");
+motor = new Component("motor");
+
 
 class Command {
   constructor(id, code){
     this.id = id;
-    this.code = code;
+    this.code = code ? code : newId();
   }
 }
 
-class Component {
-  constructor(nombre, id){
-    this.nombre = nombre;
-    this.id = id;
-    this.events = {}
-  }
+class CanBus  {
 
-  addEvent(e, command){
-    this.events[command.code] = e;
-  }
-
-  triger (command){
-    if(this.id == command.id && this.events[command.code]){
-      this.events[command.code].launch(this);
-    }
-  }
-}
-
-class Monitor {
-  constructor (){
+  constructor(){
     this.components = [];
   }
 
-  addComponent(component){
-    this.components.push(component);
+  connectComponent(c) {
+    c.setCon(this);
+    this.components.push(c);
   }
 
-  trigger(c){
-    for(let i in this.components)
-      this.components[i].triger(c);
+  w (command) {
+
+    console.log(["CANBUS"], command.id, command.code);
+    for(let i in this.components){
+      this.components[i].w(command);
+    }
   }
 
 }
 
-m = new Monitor();
-puerta = new Component('puerta_izda', 0);
-c = new Command(0, 0);
-abrir_puerta = new Event('abrir', function(puerta){
-  console.log("abierta", puerta.nombre);
+abre_puerta_izda = new Command(puerta_izda.id);
+notifica_apertura_puerta_izda = new Command(pantalla.id);
+cierra_puerta_izda = new Command(puerta_izda.id);
+notifica_cierre_puerta_izda = new Command(pantalla.id);
+enciende_motor = new Command(motor.id);
+notifica_motor_encendido = new Command(pantalla.id);
+apaga_motor = new Command(motor.id);
+notifica_motor_apagado = new Command(pantalla.id);
+
+
+puerta_izda.addEvent(abre_puerta_izda, function(x){
+  x.send(notifica_apertura_puerta_izda);
 });
 
-puerta.addEvent(abrir_puerta, c);
-m.addComponent(puerta);
-m.trigger(c);
+puerta_izda.addEvent(cierra_puerta_izda, function(x){
+  x.send(notifica_cierre_puerta_izda);
+});
 
-console.log(m.components);
+pantalla.addEvent(notifica_apertura_puerta_izda, function(x){
+  console.log([x.name], "Abierta puerta izda");
+});
+pantalla.addEvent(notifica_cierre_puerta_izda, function(x){
+  console.log([x.name], "Abierta puerta izda");
+});
+
+motor.addEvent(enciende_motor, function(x){
+  x.send(notifica_motor_encendido);
+});
+motor.addEvent(apaga_motor, function(x){
+  x.send(notifica_motor_apagado);
+});
+
+pantalla.addEvent(notifica_motor_encendido, function(x){
+  console.log([x.name], "Motor encendido");
+});
+pantalla.addEvent(notifica_motor_apagado, function(x){
+  console.log([x.name], "Motor apagado");
+});
+
+cb = new CanBus();
+cb.connectComponent(puerta_izda);
+cb.connectComponent(pantalla);
+cb.connectComponent(motor);
+
+function entra(cb){
+  cb.w(abre_puerta_izda);
+  cb.w(cierra_puerta_izda);
+}
+function arrancaCoche(cb){
+  cb.w(enciende_motor);
+}
+function paraCoche(cb){
+  cb.w(apaga_motor);
+}
+
+function sale(cb){
+  cb.w(abre_puerta_izda);
+  cb.w(cierra_puerta_izda);
+}
+
+entra(cb);
+sale(cb);
